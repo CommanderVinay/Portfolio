75052c1d00585c2ddb65b7e40151acd76c1c2675
CommanderVinay <137443982+CommanderVinay@users.noreply.github.com>
2025-08-31 20:15:04 +0530

    Fontocine project is added
    Fontocine project is added

--- moreProject.js
+++ moreProject.js
@@ -1,23 +1,20 @@
 // Toggle the dropdown menu
-        function more()
-       {
-        if( document.getElementById("projectCard5").style.display==="none")
-        {
-           
-            document.getElementById("moreBtn").innerHTML='Less <i class="fas fa-chevron-up"> </i>';
-            document.getElementById("projectCard5").style.display="block";
-            document.getElementById("projectCard6").style.display="block";
-            document.getElementById("projectCard7").style.display="block";
-            
-        }
-    else
-    {
-           
-            document.getElementById("projectCard5").style.display="none";
-            document.getElementById("projectCard6").style.display="none";
-            document.getElementById("projectCard7").style.display="none";
-            document.getElementById("moreBtn").innerHTML = 'More Projects <i class="fas fa-chevron-down"></i>';
+      function more() {
+    const card5 = document.getElementById("projectCard5");
+    const isHidden = window.getComputedStyle(card5).display === "none";
 
-        }
-       }
+    if (isHidden) {
+        document.getElementById("moreBtn").innerHTML = 'Less <i class="fas fa-chevron-up"></i>';
+        document.getElementById("projectCard5").style.display = "block";
+        document.getElementById("projectCard6").style.display = "block";
+        document.getElementById("projectCard7").style.display = "block";
+        document.getElementById("projectCard8").style.display = "block";
+    } else {
+        document.getElementById("projectCard5").style.display = "none";
+        document.getElementById("projectCard6").style.display = "none";
+        document.getElementById("projectCard7").style.display = "none";
+        document.getElementById("projectCard8").style.display = "none";
+        document.getElementById("moreBtn").innerHTML = 'More Projects <i class="fas fa-chevron-down"></i>';
+    }
+}
       
